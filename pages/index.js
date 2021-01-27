b0f4ce601f0feb25ef0a0c3e457a01285159a77e
Carlos Henrique <kiquecz328@gmail.com>
2021-01-27 17:51:12 -0300

    2 dia finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
@@ -25,6 +28,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -35,6 +41,24 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form
+              onSubmit={function (e) {
+                e.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('Fazendo uma submissão por meio do react');
+              }}
+            >
+              <input
+                placeholder="Diz ai seu nome"
+                onChange={function (e) {
+                  setName(' ' + e.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
